d9ba06db34f55bb483d71f3a67cebf10cad94b9f
Russ Troester <russtroester@MacMini.local>
2016-02-29 20:22:52 -0600

    Added files to repository

--- BombDisplay/ViewController.swift
+++ BombDisplay/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func resetDisplay(sender: AnyObject) {
+        blueBomb.hidden = false
+        redBomb.hidden = false
+    }
+    
+    @IBAction func hideBlueBomb(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
 
+    @IBAction func hideRedBomb(sender: AnyObject) {
+        redBomb.hidden = true
+    }
 }
 
